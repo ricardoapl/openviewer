ffd09afbdd5e411af80bde0428312fb1096df587
cat5e <2171496@my.ipleiria.pt>
2020-03-27 15:13:14 +0000

    apply us01 - login

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from 'vuelidate';
+import axios from 'axios';
 
+
+Vue.use(Vuelidate);
 Vue.config.productionTip = false
+
+window.axios = axios;
+
 
 new Vue({
   router,
