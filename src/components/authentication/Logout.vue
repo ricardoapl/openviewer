865dc9a38b2d8527239b4acda118c32e447d4507
cat5e <2171496@my.ipleiria.pt>
2020-03-30 13:31:39 +0100

    [US04][US05]: Switch projects & Logout & Minor visual changes

---
+++ src/components/authentication/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app"></div>
+</template>
+
+<script>
+export default {
+  created () {
+    axios
+      .delete('/identity/v3/auth/tokens', { headers: { 'X-Subject-Token': this.$store.state.tokenScoped } })
+      .then(response => {
+        this.$store.commit('clearAll')
+        this.$router.go({ name: 'Home' })
+      })
+      .catch(error => {
+        console.log('[Logout] => ' + error)
+        this.$store.commit('clearAll')
+        this.$router.go({ name: 'Home' })
+      })
+  }
+}
+</script>
