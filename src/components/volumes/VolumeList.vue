f66105fb1c6752f3576639d1ac6279a031fd45da
cat5e <2171496@my.ipleiria.pt>
2020-04-02 11:50:34 +0100

    [US23] - Applied

---
+++ src/components/volumes/VolumeList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container table-responsive">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Size</th>
+          <th>Status</th>
+          <th>Attached?</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          is="volume-list-item"
+          v-for="volume in volumes"
+          v-bind:key="volume.id"
+          v-bind:volume="volume"
+          >
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import VolumeListItem from './VolumeListItem'
+export default {
+  components: {
+    VolumeListItem
+  },
+  data () {
+    return {
+      volumes: []
+    }
+  },
+  mounted () {
+    this.getVolumes()
+  },
+  methods: {
+    getVolumes: function () {
+      axios.get('/volume/v3/' + this.$store.state.idSelectedProject + '/volumes')
+        .then(response => {
+          if (response.data.volumes.length > 0) {
+            $.each(response.data.volumes, (key, value) => {
+              this.getVolumeData(value.id);
+            })
+          }
+        })
+        .catch(error => {
+          console.log('[VolumeList]  => ' + error)
+        })
+    },
+    getVolumeData: function (idVolume) {
+      axios.get('/volume/v3/' + this.$store.state.idSelectedProject + '/volumes/' + idVolume)
+        .then(response => {
+           this.volumes.push(response.data.volume);
+        })
+        .catch(error => {
+          console.log('[VolumeList]  => ' + error)
+        })
+    },
+  }
+}
+</script>
---
+++ src/components/volumes/VolumeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="jumbotron jumbotron-fluid text-center">
+      <div class="container">
+        <h1 class="display-4">Volumes</h1>
+        <p>In this page your can control all of your volumes. <br>If your want to create a new volume click on the button down bellow.</p>
+      </div>
+      <button
+      class="btn btn-outline-info mt-4"
+      v-on:click="showVolumeForm = !showVolumeForm"
+      >- Create Volume - 
+    </button>
+    </div>
+    
+    <volume-form v-if="showVolumeForm"></volume-form>
+    <volume-list></volume-list>
+  </div>
+</template>
+
+<script>
+import VolumeForm from './VolumeForm'
+import VolumeList from './VolumeList'
+export default {
+  components: {
+    VolumeForm,
+    VolumeList
+  },
+  data () {
+    return {
+      showVolumeForm: false
+    }
+  },
+}
+</script>
