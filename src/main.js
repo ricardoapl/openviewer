f11663bf910672be6694d3dc9273952c8c7e0c53
ricardoapl <ricardoapl@protonmail.com>
2020-05-08 17:55:12 +0100

    Comment router while login isn't implemented

--- src/main.js
+++ src/main.js
@@ -10,31 +10,32 @@ Vue.config.productionTip = false
 
 window.axios = axios
 
-// TODO Remove after login is implemented
+// TODO (ricardoapl) Remove after login is implemented
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 
-// XXX Try to move this into router/index.js
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (store.state.authentication.tokenUnscoped === '') {
-      next({
-        name: 'LoginUnscoped'
-      })
-    } else {
-      next()
-    }
-  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
-    if (store.state.authentication.tokenUnscoped !== '') {
-      next({
-        name: 'Home'
-      })
-    } else {
-      next()
-    }
-  } else {
-    next()
-  }
-})
+// TODO (ricardoapl) Uncomment the following code after login is implemented
+// XXX (ricardoapl) Try to move this into router/index.js
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     if (store.state.authentication.tokenUnscoped === '') {
+//       next({
+//         name: 'LoginUnscoped'
+//       })
+//     } else {
+//       next()
+//     }
+//   } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+//     if (store.state.authentication.tokenUnscoped !== '') {
+//       next({
+//         name: 'Home'
+//       })
+//     } else {
+//       next()
+//     }
+//   } else {
+//     next()
+//   }
+// })
 
 new Vue({
   router,
