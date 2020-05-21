7e120bbb11cc68212a87c381be6027c3ff6d89f3
ricardoapl <ricardoapl@protonmail.com>
2020-05-21 18:09:18 +0100

    Fix pagination bar position

--- src/components/kubernetes/pods/ContainersList.vue
+++ src/components/kubernetes/pods/ContainersList.vue
@@ -1,94 +1,110 @@
 <template>
-    <span>
-        <span v-if="pod && pod.spec.containers.length>0">
-            <div  class="container table-responsive">
-            <b-table show-empty bordered :per-page="perPage" :current-page="currentPage" striped hover :items="pod.spec.containers" :fields="fields">
-                <template v-slot:cell(memory)="row">
-                        <span v-if="row.item.resources.limits">{{row.item.resources.limits.memory|| 'None'}}</span>
-                        <span v-else>None</span>
-                    </template>
-                    <template v-slot:cell(cpu)="row">
-                        <span v-if="row.item.resources.limits">{{row.item.resources.limits.cpu || 'None'}}</span>
-                        <span v-else>None</span>
-                    </template>
-            </b-table>
-            </div>
-            <b-pagination
-                v-model="currentPage"
-                :total-rows="rows"
-                :per-page="perPage"
-                aria-controls="my-table"
-                align="right"
-            ></b-pagination>
-        </span>
-        <span v-else>
-            <div  class="container table-responsive">
-            <b-table show-empty bordered :per-page="perPage" :current-page="currentPage" striped hover :items="[]" :fields="fields">
-            </b-table>
-            </div>
-        </span>
+  <span>
+    <span v-if="pod && pod.spec.containers.length>0">
+      <div class="container table-responsive">
+        <b-table
+          show-empty
+          bordered
+          :per-page="perPage"
+          :current-page="currentPage"
+          striped
+          hover
+          :items="pod.spec.containers"
+          :fields="fields"
+        >
+          <template v-slot:cell(memory)="row">
+            <span v-if="row.item.resources.limits">{{ row.item.resources.limits.memory|| 'None' }}</span>
+            <span v-else>None</span>
+          </template>
+          <template v-slot:cell(cpu)="row">
+            <span v-if="row.item.resources.limits">{{ row.item.resources.limits.cpu || 'None' }}</span>
+            <span v-else>None</span>
+          </template>
+        </b-table>
+
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          aria-controls="my-table"
+          align="right"
+        />
+      </div>
     </span>
-  
+    <span v-else>
+      <div class="container table-responsive">
+        <b-table
+          show-empty
+          bordered
+          :per-page="perPage"
+          :current-page="currentPage"
+          striped
+          hover
+          :items="[]"
+          :fields="fields"
+        />
+      </div>
+    </span>
+  </span>
 </template>
 
 <script>
 
-
 export default {
   name: 'ContainersList',
-  props:[
-    'pod',
+  props: [
+    'pod'
   ],
   data () {
     return {
       perPage: 5,
       currentPage: 1,
-      selectedPod:{},
-      fields:[
+      selectedPod: {},
+      fields: [
         {
-          key:'name',
-          label:'Name'
+          key: 'name',
+          label: 'Name'
         },
         {
-          key:'image',
-          label:'Image'
+          key: 'image',
+          label: 'Image'
         },
         {
-          key:'memory',
-          label:'Memory Limit'
+          key: 'memory',
+          label: 'Memory Limit'
         },
         {
-          key:'cpu',
-          label:'CPU Limit'
-        },
-      ],
-      
+          key: 'cpu',
+          label: 'CPU Limit'
+        }
+      ]
+
     }
-  },
-  mounted () {
-      console.log(this.pod.spec)
   },
   computed: {
     filteredPods () {
-      if(this.namespace=='*'){
-        return this.$store.state.pods.pods;
-      } else{
-        return this.$store.state.pods.pods.filter((obj)=>{
-          return obj.metadata.namespace == this.namespace;
+      if (this.namespace == '*') {
+        return this.$store.state.pods.pods
+      } else {
+        return this.$store.state.pods.pods.filter((obj) => {
+          return obj.metadata.namespace == this.namespace
         })
       }
     },
-    rows() {
-      return this.filteredPods.length;
-    },
-  },
-  methods: {
-    onRowSelected(items) {
-        this.selectedPod = items[0];
-        this.$emit('newSelectedPod',this.selectedPod);
-    },
+    rows () {
+      return this.filteredPods.length
+    }
   },
   watch: {
+  },
+  mounted () {
+    console.log(this.pod.spec)
+  },
+  methods: {
+    onRowSelected (items) {
+      this.selectedPod = items[0]
+      this.$emit('newSelectedPod', this.selectedPod)
+    }
   }
 }
 </script>
