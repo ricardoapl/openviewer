38648863928f9b3b60800d1209961e4fe975ea5e
ricardoapl <ricardoapl@protonmail.com>
2020-05-22 11:13:55 +0100

    Add ports for multiple container deployment creation

--- src/components/kubernetes/deployments/DeploymentsFormBasic.vue
+++ src/components/kubernetes/deployments/DeploymentsFormBasic.vue
@@ -18,11 +18,11 @@
               required
             />
           </b-form-group>
-          <!-- XXX (ricardoapl) To what does label-for point to? -->
+          <!-- XXX (ricardoapl) To what does label-for point to? Nothing... -->
           <b-form-group
-            id="images-input-group"
-            label="Images"
-            label-for="images"
+            id="containers-input-group"
+            label="Containers"
+            label-for="containers"
           >
             <div
               v-for="(container, index) in containers"
@@ -50,14 +50,23 @@
                     />
                   </svg>
                 </b-button>
-                <div class="mr-1 col-2">
+                <b-col role="group">
                   <b-form-input
                     v-model="container.image"
-                    class="form-control-sm"
                     placeholder="Enter image name"
+                    type="text"
                     required
                   />
-                </div>
+                </b-col>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="container.ports[0].containerPort"
+                    placeholder="Enter container port"
+                    type="number"
+                    min="1"
+                    max="65535"
+                  />
+                </b-col>
               </span>
             </div>
             <b-button
@@ -96,30 +105,88 @@
               max="10"
             />
           </b-form-group>
-          <!-- XXX (ricardoapl) What about deployments with multiple ports? -->
-          <b-form-group id="service-input-group">
-            <b-form-row>
-              <b-col role="group">
-                <label for="service-port">Service Port</label>
-                <b-form-input
-                  id="service-port"
-                  v-model="servicePort"
-                  type="number"
-                  min="1024"
-                  max="65535"
+          <b-form-group
+            id="service-input-group"
+            label="Service (Ports)"
+            label-for="service-ports"
+          >
+            <div
+              v-for="(port, index) in servicePorts"
+              :key="port.name"
+            >
+              <span class="mx-auto row mb-2">
+                <b-button
+                  class="mx-1"
+                  size="sm"
+                  variant="danger"
+                  @click="removeServicePort(index)"
+                >
+                  <svg
+                    class="bi bi-trash-fill"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </b-button>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="port.port"
+                    placeholder="Enter service port"
+                    type="number"
+                    min="1024"
+                    max="65535"
+                    required
+                  />
+                </b-col>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="port.targetPort"
+                    placeholder="Enter target port"
+                    type="number"
+                    min="1"
+                    max="65535"
+                    required
+                  />
+                </b-col>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="port.protocol"
+                    placeholder="Enter protocol"
+                    type="text"
+                    required
+                  />
+                </b-col>
+              </span>
+            </div>
+            <b-button
+              class="mx-1"
+              size="sm"
+              variant="success"
+              @click="addServicePort()"
+            >
+              <svg
+                class="bi bi-plus-square-fill"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm6.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z"
+                  clip-rule="evenodd"
                 />
-              </b-col>
-              <b-col role="group">
-                <label for="container-port">Container Port</label>
-                <b-form-input
-                  id="container-port"
-                  v-model="containerPort"
-                  type="number"
-                  min="1"
-                  max="65535"
-                />
-              </b-col>
-            </b-form-row>
+              </svg>
+            </b-button>
           </b-form-group>
           <!-- XXX (ricardoapl) What about labels? -->
           <div class="text-center">
@@ -164,10 +231,13 @@ export default {
           }
         ]
       }],
-      replicas: 3,
-      // XXX (ricardoapl) Service parameters...
-      servicePort: 5555,
-      containerPort: 80
+      replicas: 1,
+      servicePorts: [{
+        name: 'port0',
+        port: 5555,
+        targetPort: 80,
+        protocol: 'TCP'
+      }]
     }
   },
   mounted () {
@@ -177,10 +247,10 @@ export default {
     addContainer: function () {
       const container = {
         name: `image${this.containers.length}`,
-        image: `image:1.${this.containers.length}`,
+        image: '',
         ports: [
           {
-            containerPort: 80
+            containerPort: null
           }
         ]
       }
@@ -189,6 +259,20 @@ export default {
     removeContainer: function (index) {
       if (index > -1) {
         this.containers.splice(index, 1)
+      }
+    },
+    addServicePort: function () {
+      const servicePort = {
+        name: `port${this.servicePorts.length}`,
+        port: null,
+        targetPort: null,
+        protocol: ''
+      }
+      this.servicePorts.push(servicePort)
+    },
+    removeServicePort: function (index) {
+      if (index > -1) {
+        this.servicePorts.splice(index, 1)
       }
     },
     createDeployment: function () {
@@ -234,6 +318,8 @@ export default {
         }
       }
       this.containers.forEach(function (container) {
+        // XXX (ricardoapl) HTML always returns text... We should parse somewhere else
+        container.ports[0].containerPort = parseInt(container.ports[0].containerPort)
         body.spec.template.spec.containers.push(container)
       })
       return body
@@ -262,19 +348,21 @@ export default {
           selector: {
             app: this.name
           },
-          ports: [
-            {
-              port: parseInt(this.servicePort),
-              targetPort: parseInt(this.containerPort)
-            }
-          ]
+          ports: []
         }
       }
+      this.servicePorts.forEach(function (servicePort) {
+        // XXX (ricardoapl) HTML always returns text... We should parse somewhere else
+        servicePort.port = parseInt(servicePort.port)
+        servicePort.targetPort = parseInt(servicePort.targetPort)
+        body.spec.ports.push(servicePort)
+      })
       return body
     },
     submitForm: function () {
       // XXX (ricardoapl) Might want to remove 'hide' event chain
       // XXX (ricardoapl) Delete existing services with same name?
+      // XXX (ricardoapl) What happens if we don't fill service form?
       this.createDeployment()
         .then(() => {
           this.createService()
