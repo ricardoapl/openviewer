07f48797c5f82cb07c8cf7f6e98a42aba6b1fdef
ricardoapl <ricardoapl@protonmail.com>
2020-05-22 11:44:36 +0100

    Add multiple containers (and ports) to deployment edit

--- src/components/kubernetes/deployments/DeploymentsListActionEditBasic.vue
+++ src/components/kubernetes/deployments/DeploymentsListActionEditBasic.vue
@@ -17,17 +17,77 @@
               v-model="name"
             />
           </b-form-group>
-          <!-- XXX (ricardoapl) What about deployments with multiple containers (and images)? -->
           <b-form-group
-            id="image-input-group"
-            label="Image"
-            label-for="image"
+            id="containers-input-group"
+            label="Containers"
+            label-for="containers"
           >
-            <b-form-input
-              id="image"
-              v-model="image"
-              required
-            />
+            <div
+              v-for="(container, index) in containers"
+              :key="container.name"
+            >
+              <span class="mx-auto row mb-2">
+                <b-button
+                  class="mx-1"
+                  size="sm"
+                  variant="danger"
+                  @click="removeContainer(index)"
+                >
+                  <svg
+                    class="bi bi-trash-fill"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </b-button>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="container.image"
+                    placeholder="Enter image name"
+                    type="text"
+                    required
+                  />
+                </b-col>
+                <b-col role="group">
+                  <b-form-input
+                    v-model="container.ports[0].containerPort"
+                    placeholder="Enter container port"
+                    type="number"
+                    min="1"
+                    max="65535"
+                  />
+                </b-col>
+              </span>
+            </div>
+            <b-button
+              class="mx-1"
+              size="sm"
+              variant="success"
+              @click="addContainer()"
+            >
+              <svg
+                class="bi bi-plus-square-fill"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm6.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </b-button>
           </b-form-group>
           <b-form-group
             id="replicas-input-group"
@@ -42,32 +102,6 @@
               min="1"
               max="10"
             />
-          </b-form-group>
-          <!-- XXX (ricardoapl) What about deployments with multiple ports? -->
-          <b-form-group id="service-input-group">
-            <b-form-row>
-              <b-col role="group">
-                <label for="service-port">Service Port</label>
-                <b-form-input
-                  id="service-port"
-                  v-model="servicePort"
-                  disabled
-                  type="number"
-                  min="1024"
-                  max="65535"
-                />
-              </b-col>
-              <b-col role="group">
-                <label for="container-port">Container Port</label>
-                <b-form-input
-                  id="container-port"
-                  v-model="containerPort"
-                  type="number"
-                  min="1"
-                  max="65535"
-                />
-              </b-col>
-            </b-form-row>
           </b-form-group>
           <!-- XXX (ricardoapl) What about labels? -->
         </div>
@@ -103,11 +137,8 @@ export default {
   data () {
     return {
       name: this.deployment.metadata.name,
-      image: this.deployment.spec.template.spec.containers[0].image,
-      replicas: this.deployment.spec.replicas,
-      // XXX (ricardoapl) Not used at the moment...
-      servicePort: null,
-      containerPort: this.deployment.spec.template.spec.containers[0].ports[0].containerPort
+      containers: this.deployment.spec.template.spec.containers,
+      replicas: this.deployment.spec.replicas
     }
   },
   mounted () {
@@ -116,6 +147,23 @@ export default {
     $(function () { $('[data-toggle="tooltip"]').tooltip() })
   },
   methods: {
+    addContainer: function () {
+      const container = {
+        name: `image${this.containers.length}`,
+        image: '',
+        ports: [
+          {
+            containerPort: null
+          }
+        ]
+      }
+      this.containers.push(container)
+    },
+    removeContainer: function (index) {
+      if (index > -1) {
+        this.containers.splice(index, 1)
+      }
+    },
     saveDeployment: function () {
       const namespace = this.deployment.metadata.namespace
       const deployment = this.deployment.metadata.name
@@ -141,9 +189,12 @@ export default {
       Object.assign(body, this.deployment)
       // XXX (ricardoapl) Ewwwww! Code smell!
       body.metadata.name = this.name
-      body.spec.template.spec.containers[0].image = this.image
       body.spec.replicas = parseInt(this.replicas)
-      body.spec.template.spec.containers[0].ports[0].containerPort = parseInt(this.containerPort)
+      this.containers.forEach(function (container) {
+        // XXX (ricardoapl) HTML always returns text... We should parse somewhere else
+        container.ports[0].containerPort = parseInt(container.ports[0].containerPort)
+      })
+      body.spec.template.spec.containers = this.containers
       return body
     }
   }
